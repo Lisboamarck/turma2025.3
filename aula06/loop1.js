7ac2ea975fa2a0996be10c8b156651a07d43b902
Lisboamarck <mp-1997@hotmail.com>
2025-10-06 18:49:01 -0300

    aulas 06/10
    exercicios

---
+++ aula06/loop1.js
@@ -0,0 +1,10 @@
+// contar de 1 a 10 usando while
+let i = 1;
+while (i<= 10){ 
+    console('contando com while..',i);
+    i++ // aumentando valor de i
+}
+console.log('-=-=-=-=-=-=-=-=-=-');
+for(let i=1; i<=10; i++){
+    console.log('contando com for..',i);
+}
---
+++ aula06/loop2.js
@@ -0,0 +1,10 @@
+// contar de 1 a 10 usando while
+let i = 10;
+while (i>=1){ 
+    console.log('contando com while..',i);
+    i-- //diminuindo o valor de i
+}
+console.log('-=-=-=-=-=-=-=-=-=-=-');
+for(let i=10; i>=1; i--){
+    console.log('contando com for..',i);
+}
